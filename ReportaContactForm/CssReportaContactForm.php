c837fba6def54d09aa0ddad26294e6e6e3b70500
vimbai-dotcom <vimbaikurewa@gmail.com>
2021-04-08 09:17:31 +0100

    check commit

---
+++ ReportaContactForm/CssReportaContactForm.php
@@ -0,0 +1,79 @@
+nav{
+    padding:10px;
+    background:#eb5757;
+}
+nav a{
+    text-decoration: none;
+    color:white;
+    font-weight: bolder;
+    font-size:24px;
+    padding-left:1.7rem;
+    margin-left:3rem;
+}
+body{
+    margin:0;
+}
+label{
+    display:block;
+    color:black;
+    font-weight: bolder;
+    font-size:20px;
+}
+
+#btn{
+border-radius: 3px;
+padding:5px 20px;
+background:white;
+color:#eb5757;
+border:none;
+font-weight:bolder;
+border:2px solid #eb5757;
+}
+#form-block{
+    padding:1px 9px;
+    width:auto;
+}
+input[type=text]{
+    border-radius: 2px;
+    padding:5px 10px;
+}
+.form-message{
+    text-align: left;
+    padding-top:2rem;
+    margin-top:2rem;
+    font-weight: bolder;
+    padding-left:2rem;
+}
+.form-message h1{
+    text-decoration-style: solid;
+    font-size:34px;
+
+}
+.form-message h3{
+    font-size:20px;
+    font-weight: lighter;
+    line-height: 40px;
+    letter-spacing: 1px;
+}
+fieldset{
+background:#eb5757;
+padding:7px;
+border:none;
+padding-left:2rem;
+padding-top:2rem;
+}
+@media only screen and(min-width:1080px), only screen and (min-width:700px){
+    .form-field{
+        width:40%;
+    }
+
+}
+@media only screen and (max-width: 600px){
+    .fieldset{
+        width:60%;
+    }
+    .message-1{
+    padding-top:1rem;
+    margin-top:none;
+}
+}
---
+++ ReportaContactForm/HtmlReportaContactForm.php
@@ -0,0 +1,50 @@
+!DOCTYPE html>
+<html lang= 'en'>
+<head>
+	<meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Contact-Us</title>
+	<link rel="stylesheet" href='style.css'>
+</head>
+<body>
+<nav>
+	<a href="#" class="brand-logo">Reporta</a>
+</nav>
+<section class='container-1'>
+	<div class="form-message message-1">
+		<h1>Connect with us</h1>
+		<h3>We would love to respond to your queries,feel free to get in touch with us.</h3>
+	</div>
+	<div class="form">
+		<br><br>
+		<form name='myForm' id='form-block'>
+			<fieldset class="fieldset form-field">
+			<div class='form-container'>
+			<label for ='name'>Name:</label>
+			<input type='text' name='text'>
+			<br>
+			<br>
+				<label for ="question">Are you a registered customer?</label>
+				<input type='text' name='text'>
+				<br><br>
+				<label for ='phone'>Phone:</label>
+				<input type='text' name='text'>
+				<br><br>
+				<label for ='email'>Email:</label>
+				<input type='text' name='text'>
+				<br><br>
+				<label for ='subject'>Subject:</label>
+				<textarea rows='6' cols='30'></textarea>
+				<br>
+				<br>
+				<button id='btn'>Send</button>
+			</div>
+		</fieldset>
+		</form>
+	</div>
+</section>
+
+<div class='container'>
+</div>
+</body>
+</html>
